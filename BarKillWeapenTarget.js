c3c20459aac5fb84c55a62111bca2d6a6e9a2517
SanneKerck <81096871+SanneKerck@users.noreply.github.com>
2021-05-05 23:03:59 +0200

    Update BarKillWeapenTarget.js

--- BarKillWeapenTarget.js
+++ BarKillWeapenTarget.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 var margin = {top: 20, right: 30, bottom: 160, left: 70},
 width = 800 - margin.left - margin.right,
-height = 800 - margin.top - margin.bottom;
+height = 700 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
